52279a96a8ceb8423e13d321edbbbbd60e21d754
duccv0209 <caovanduca2@gmail.com>
2019-12-17 11:22:33 +0700

    fix server (localhost)

--- server.go
+++ server.go
@@ -17,7 +17,7 @@ const commandLength = 12
 
 var nodeAddress string
 var miningAddress string
-var knownNodes = []string{"192.168.43.213:3000"}
+var knownNodes = []string{"localhost:3000"}
 var blocksInTransit = [][]byte{}
 var mempool = make(map[string]Transaction)
 
@@ -490,7 +490,7 @@ func handleConnection(conn net.Conn, bc *Blockchain) {
 
 // StartServer starts a node
 func StartServer(nodeID, minerAddress string) {
-	nodeAddress = fmt.Sprintf("192.168.43.98:%s", nodeID)
+	nodeAddress = fmt.Sprintf("localhost:%s", nodeID)
 	miningAddress = minerAddress
 	ln, err := net.Listen(protocol, nodeAddress)
 	if err != nil {
